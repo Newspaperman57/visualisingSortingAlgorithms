cecd2355a05898307cb1eb418dd630a7395fc485
Mathias Pihl <mpihl15@its.aau.dk>
2017-10-25 12:22:05 +0200

    Changes greyscale to hue-scale

--- satisfaction.py
+++ satisfaction.py
@@ -1,5 +1,5 @@
 #/usr/bin/python3
-import pygame, sys, random, sortingAlgorithms, copy
+import pygame, sys, random, sortingAlgorithms, copy, colorsys
 
 pygame.init()
 
@@ -10,7 +10,7 @@ background = pygame.Surface(screen.get_size())
 background = background.convert()
 background.fill((0,0,0))
 
-pixelSize = 5
+pixelSize = 10
 gridHeight = int((900-20)/pixelSize)
 gridWidth = int((1600-20)/pixelSize)
 grid = [[0 for x in range(gridHeight)] for y in range(gridWidth)] 
@@ -42,13 +42,15 @@ while True:
             pass
         for y in range(gridHeight):
             if grid[x][y] != lastGrid[x][y] or first:
+                col = pygame.Color(0)
+                col.hsva = (grid[x][y]*360, 100, 100, 100)
                 pygame.display.update(
-                    pygame.draw.rect(screen, (grid[x][y]*256, grid[x][y]*256, grid[x][y]*256), (x*pixelSize+offsetX, y*pixelSize+offsetY, pixelSize, pixelSize)))
+                    pygame.draw.rect(screen, col, (x*pixelSize+offsetX, y*pixelSize+offsetY, pixelSize, pixelSize)))
                 lastGrid[x][y] = grid[x][y]
     
     first = False
     #pygame.display.flip()
 
-    clock.tick()
+    clock.tick(60)
     print(clock.get_fps())
     #screen.blit(background, (0,0))
