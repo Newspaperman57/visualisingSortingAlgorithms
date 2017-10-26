09f8b04da34f65390ec67943d63b8ee7af3b1a1e
Anton Christensen <anton.christensen9700@gmail.com>
2017-10-26 15:08:41 +0200

    Restarts when lists are sorted

--- satisfaction.py
+++ satisfaction.py
@@ -18,42 +18,69 @@ sorter = [0 for x in range(gridWidth)]
 offsetX = 10
 offsetY = 10
 
-sort = sortingAlgorithms.random_sort()
-for x in range(len(grid)):
-    for y in range(len(grid[x])):
-        grid[x][y] = (y/float(len(grid[x])))
-    random.shuffle(grid[x])
-    sorter[x] = sort(grid[x])
-
-lastGrid = copy.deepcopy(grid)
-background.fill(0x000000)
+sortedLists = []
 first = True
 
 clock = pygame.time.Clock()
 simulateColorBlindness = False if len(sys.argv) == 2 and sys.argv[1] == "--no-color" else True
+
+
+
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: 
-            sys.exit()
+
+    background.fill(0x000000)
+    
+    sortedLists = [False]*len(grid)
+    sort = sortingAlgorithms.random_sort()
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            grid[x][y] = (y/float(len(grid[x])))
+        random.shuffle(grid[x])
+        sorter[x] = sort(grid[x])
+
+    lastGrid = copy.deepcopy(grid)
+
     updates = []
     for x in range(gridWidth):
-        try:
-            next(sorter[x])
-        except StopIteration as e:
-            pass
         for y in range(gridHeight):
-            if grid[x][y] != lastGrid[x][y] or first:
-                col = pygame.Color(0)
-                if simulateColorBlindness:
-                    col.hsva = (grid[x][y]*360, 100, 90, 100)
-                else:
-                    col.hsva = (0, 0, grid[x][y]*100, 100)
-                updates.append(pygame.draw.rect(screen, col, (x*pixelSize+offsetX, y*pixelSize+offsetY, pixelSize, pixelSize)))
-                lastGrid[x][y] = grid[x][y]
+            col = pygame.Color(0)
+            if simulateColorBlindness:
+                col.hsva = (grid[x][y]*360, 100, 90, 100)
+            else:
+                col.hsva = (0, 0, grid[x][y]*100, 100)
+            updates.append(pygame.draw.rect(screen, col, (x*pixelSize+offsetX, y*pixelSize+offsetY, pixelSize, pixelSize)))
+            lastGrid[x][y] = grid[x][y]
     pygame.display.update(updates)
-    first = False
-    #pygame.display.flip()
 
-    clock.tick(60)
-    #print(clock.get_fps())
-    #screen.blit(background, (0,0))
+    done = False
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                sys.exit()
+        updates = []
+        for x in range(gridWidth):
+            try:
+                next(sorter[x])
+            except StopIteration as e:
+                sortedLists[x] = True
+                done = True
+                for i in sortedLists:
+                    if i == False:
+                        done = False
+                        break
+            for y in range(gridHeight):
+                if grid[x][y] != lastGrid[x][y] or first:
+                    col = pygame.Color(0)
+                    if simulateColorBlindness:
+                        col.hsva = (grid[x][y]*360, 100, 90, 100)
+                    else:
+                        col.hsva = (0, 0, grid[x][y]*100, 100)
+                    updates.append(pygame.draw.rect(screen, col, (x*pixelSize+offsetX, y*pixelSize+offsetY, pixelSize, pixelSize)))
+                    lastGrid[x][y] = grid[x][y]
+        pygame.display.update(updates)
+        first = False
+        #pygame.display.flip()
+
+        clock.tick(60)
+        #print(clock.get_fps())
+        #screen.blit(background, (0,0))
